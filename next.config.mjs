3ec5af82674941caba636424868c3576fdea3efc
Gideon Olayode <olayode.gideon@calebuniversity.edu.ng>
2024-09-26 13:51:14 +0100

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,29 @@
+import withPWA from 'next-pwa';
+
 /** @type {import('next').NextConfig} */
+
+let configwithPWA = withPWA({
+  async headers() {
+    return [
+      {
+        source: '/manifest.json',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/manifest+json',
+          },
+        ],
+      },
+    ];
+  },
+  pwa: {
+    dest: 'public',
+  },
+});
+
 const nextConfig = {
-  reactStrictMode: true,
+  // reactStrictMode: true,
+  ...configwithPWA,
 };
 
 export default nextConfig;
