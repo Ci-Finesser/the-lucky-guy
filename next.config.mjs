eee36adbac7f75c682818d863c5a50f7c3887a91
Gideon Olayode <olayode.gideon@calebuniversity.edu.ng>
2024-10-11 17:36:23 +0100

    configwithPWA

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import withPWA from 'next-pwa';
 /** @type {import('next').NextConfig} */
 
 let configwithPWA = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
   async headers() {
     return [
       {
--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -47,10 +47,8 @@ export default function SEO({ title, description, ogImage, keywords }: SEOProps)
             <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png" />
             <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png" />
             <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png" />
-            {/* <link rel="manifest" href="/site.webmanifest" /> */}
+            <link rel="manifest" href="/manifest.json" />
             <link rel="preload" href="/fonts/urbanist-reg.ttf" as="font" type="font/ttf" crossOrigin='' />
-
-            {/* Add more meta tags as needed */}
         </Head>
     )
 }
